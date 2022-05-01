03f97d752d4b580bce2f231406fe449d40bd2274
Rezwan <rez182@gmail.com>
2022-04-30 21:03:42 -0400

    improved readability

--- rmdupvid.sh
+++ rmdupvid.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+
+# # helper function
+find_match () {
+	for vid in "${ary[@]}"
+	do
+		matchFile=$(echo "$vid" | rev | cut -f 2- -d '.' | rev)	# remove ext
+		matchFile=$(echo $matchFile | sed 's+.++')	# remove first .
+		matchFile=$(pwd)$matchFile	# append current path in front (to match with delFile)
+
+		if [[ "$1" == "$matchFile" ]]; then
+			matchFile=$(echo "$vid" | sed 's+.++')
+			function_return=$(pwd)"$matchFile"
+			return 0
+		fi
+	done
+}
+# ------------------------------------------------------------------------------------------
+
+# Optional ramdisk location
 ramdisk=""
 
 # Generate image
-# ------------------------------------------------------------------------------------------
 SAVEIFS=$IFS 	# save IFS
 IFS=$'\n'	# change IFS to new line
 function_return=''
@@ -20,44 +38,24 @@ do
 done
 
 
-# ------------------------------------------------------------------------------------------
+# Get list of duplicate images
+Dup_img_list=$(findimagedupes *.jpg)
 
-
-# # helper function
-find_match () {
-	for vid in "${ary[@]}"
-	do
-		matchFile=$(echo "$vid" | rev | cut -f 2- -d '.' | rev)	# remove ext
-		matchFile=$(echo $matchFile | sed 's+.++')	# remove first .
-		matchFile=$(pwd)$matchFile	# append current path in front (to match with delFile)
-
-		if [[ "$1" == "$matchFile" ]]; then
-			matchFile=$(echo "$vid" | sed 's+.++')
-			function_return=$(pwd)"$matchFile"
-			return 0
-		fi
-	done
-}
-
-
-# # Get list of duplicate images
-# ------------------------------------------------------------------------------------------
-files=$(findimagedupes *.jpg)
-
-# format $text to add new lines for each file:
-files=$(echo $files | sed 's+/++')	# remove the first occurance of '/'
-files=$(echo $files | sed 's+ /+\n+g')	# then remove the rest and make new lines for each file
-files=($files)	# convert newline string to array
+# format text to add new lines for each file location in list:
+Dup_img_list=$(echo $Dup_img_list | sed 's+/++')	# remove the first occurance of '/'
+Dup_img_list=$(echo $Dup_img_list | sed 's+ /+\n+g')	# then remove the rest and make new lines for each file
+Dup_img_list=($Dup_img_list)	# convert newline string to array
 
 mkdir remove
 
-for (( c=0; c<${#files[@]}; c=c+2 ))
+for (( c=0; c<${#Dup_img_list[@]}; c=c+2 ))
 do
-	delFile1=$(echo "/${files[$c]}" | rev | cut -f 2- -d '.' | rev)
-	delFile2=$(echo "/${files[$c+1]}" | rev | cut -f 2- -d '.' | rev)
+	delFile1=$(echo "/${Dup_img_list[$c]}" | rev | cut -f 2- -d '.' | rev)
+	delFile2=$(echo "/${Dup_img_list[$c+1]}" | rev | cut -f 2- -d '.' | rev)
 
 	find_match "$delFile1"
 	matchFile1="$function_return"
+	
 	find_match "$delFile2"
 	matchFile2="$function_return"
 
